99efcae73c162dd976544f1d554070ab40ea7d7a
guill <guill@GUILLERMO-PC>
2024-05-02 22:36:13 -0400

    Add project files.

---
+++ Class/VerificadorNumero.cs
@@ -0,0 +1,24 @@
+﻿
+namespace programa3._3.Class
+{
+    
+        class VerificadorNumero
+        {
+            public static string VerificarDigitos(int numero)
+            {
+                if (numero >= 10 && numero <= 99)
+                {
+                    return "El número tiene dos dígitos.";
+                }
+                else if (numero >= 1 && numero <= 9)
+                {
+                    return "El número tiene un dígito.";
+                }
+                else
+                {
+                    return "El número ingresado no es válido.";
+                }
+            }
+        }
+    }
+
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using programa3._3.Class;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int numero;
+        bool inputCorrecto = false;
+
+        do
+        {
+            Console.WriteLine("Ingrese un número entre 1 y 99:");
+            if (!int.TryParse(Console.ReadLine(), out numero))
+            {
+                Console.WriteLine("Error: Por favor ingrese un número válido.");
+            }
+            else if (numero < 1 || numero > 99)
+            {
+                Console.WriteLine("Error: El número debe estar entre 1 y 99.");
+            }
+            else
+            {
+                inputCorrecto = true;
+            }
+        } while (!inputCorrecto);
+
+        string resultado = VerificadorNumero.VerificarDigitos(numero);
+        Console.WriteLine(resultado);
+
+        Console.ReadLine();
+    }
+}
